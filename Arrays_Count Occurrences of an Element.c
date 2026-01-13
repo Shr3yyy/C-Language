ffdcd2f8632c152aaf53b3910d1737f662cad57e
SHREYANSH BHARDWAJ <shreyyy3126@gmail.com>
2026-01-14 00:42:37 +0530

    Create Arrays_Count Occurrences of an Element.c

---
+++ Arrays_Count Occurrences of an Element.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;++i){
+        scanf("%d",&a[i]);
+    }
+    int target;
+    scanf("%d",&target);
+    int count=0;
+    for(int i=0;i<n;++i){
+        if(target==a[i]){
+            count++;
+        }
+    }
+    printf("%d",count);
+    return 0;
+}
